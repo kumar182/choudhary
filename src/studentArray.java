ccb7c0f498f5afc46a0b1702616fd2ad49ed382a
sanju <sanju@10.48.49.17>
2019-01-22 19:28:01 -0500

    this is trial repository

---
+++ src/studentArray.java
@@ -0,0 +1,31 @@
+
+import student.Student;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sanju
+ */
+public class studentArray {
+    
+
+
+
+    public static void main(String[] args)
+    {
+        char[] ch=new char[5];
+        Student[] s=new Student[5];
+        s[0].id=10;
+        s[0].name="Megha";
+    
+    
+}}
+
+
+
+
